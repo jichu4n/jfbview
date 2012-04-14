a20026994177f03164d911cac9f446926eaf5448
Chuan Ji <jichuan89@gmail.com>
2012-04-14 18:13:14 -0400

    Horizontally center pages less wide than screen.

--- fbpdf.c
+++ fbpdf.c
@@ -44,9 +44,9 @@ static int page_cols = PDFCOLS; /* actual height of current page in pixels */
 
 static void draw(void)
 {
-	int i;
+        int i, start_col = MAX((fb_cols() - page_cols) >> 1, 0);
 	for (i = head; i < MIN(head + fb_rows(), page_rows); i++)
-		fb_set(i - head, 0,
+		fb_set(i - head, start_col,
                        ((void *)pbuf) + (i * PDFCOLS + left) * bpp,
                        fb_cols());
 }
