6b485b07a1a4e4f775def20f9eac7a09c903ac67
Chuan Ji <jichuan89@gmail.com>
2012-05-06 19:30:09 -0400

    Update to MuPDF v1.0 API.

--- mupdf.c
+++ mupdf.c
@@ -6,15 +6,15 @@
 #include "doc.h"
 
 struct doc {
-	fz_glyph_cache *glyphcache;
-	pdf_xref *xref;
-        int rows, cols;
-        struct outline *outline;
+  fz_context *context;
+  pdf_document *document;
+  int rows, cols;
+  struct outline *outline;
 };
 
-/* Convert pdf_outline to outline_iterator. */
+/* Convert fz_outline to outline_iterator. */
 static struct outline *convert_outline_impl(
-    pdf_outline *src, struct outline *parent, struct outline *prev, int depth) {
+    fz_outline *src, struct outline *parent, struct outline *prev, int depth) {
   if (src == NULL) {
     return NULL;
   }
@@ -23,7 +23,7 @@ static struct outline *convert_outline_impl(
   dest->depth = depth;
   dest->parent = parent;
   dest->prev = prev;
-  dest->first_child = convert_outline_impl(src->child, dest, NULL, depth + 1);
+  dest->first_child = convert_outline_impl(src->down, dest, NULL, depth + 1);
   dest->last_child = dest->first_child;
   while (dest->last_child && dest->last_child->next) {
     dest->last_child = dest->last_child->next;
@@ -34,8 +34,8 @@ static struct outline *convert_outline_impl(
   return dest;
 }
 
-/* Convert pdf_outline to outline_iterator. */
-static struct outline *convert_outline(pdf_outline *src) {
+/* Convert fz_outline to outline_iterator. */
+static struct outline *convert_outline(fz_outline *src) {
   return convert_outline_impl(src, NULL, NULL, 0);
 }
 
@@ -49,92 +49,88 @@ static void free_outline(struct outline *it) {
 }
 
 void *doc_draw(struct doc *doc, int p, int zoom, int rotate) {
-	fz_matrix ctm;
-	fz_bbox bbox;
-	fz_pixmap *pix;
-	fz_device *dev;
-	fz_display_list *list;
-	pdf_page *page;
-	int x, y;
-        int bpp = FBM_BPP(fb_mode());
-        void *buffer = NULL;
-        int buffer_size = 0;
+  fz_matrix ctm;
+  fz_bbox bbox;
+  fz_pixmap *pix;
+  fz_device *dev;
+  fz_display_list *list;
+  pdf_page *page;
+  int x, y;
+  int bpp = FBM_BPP(fb_mode());
+  void *buffer = NULL;
+  int buffer_size = 0;
 
-	if (pdf_load_page(&page, doc->xref, p - 1))
-		return NULL;
-	list = fz_new_display_list();
-	dev = fz_new_list_device(list);
-	if (pdf_run_page(doc->xref, page, dev, fz_identity))
-		return NULL;
-	fz_free_device(dev);
+  if ((page = pdf_load_page(doc->document, p - 1)))
+    return NULL;
+  list = fz_new_display_list(doc->context);
+  dev = fz_new_list_device(doc->context, list);
+  pdf_run_page(doc->document, page, dev, fz_identity, NULL);
+  fz_free_device(dev);
 
-	ctm = fz_translate(0, -page->mediabox.y1);
-	ctm = fz_concat(ctm, fz_scale((float) zoom / 10, (float) -zoom / 10));
-	if (rotate)
-		ctm = fz_concat(ctm, fz_rotate(rotate));
-	bbox = fz_round_rect(fz_transform_rect(ctm, page->mediabox));
+  ctm = fz_translate(0, -pdf_bound_page(doc->document, page).y1);
+  ctm = fz_concat(ctm, fz_scale((float) zoom / 10, (float) -zoom / 10));
+  if (rotate)
+    ctm = fz_concat(ctm, fz_rotate(rotate));
+  bbox = fz_round_rect(fz_transform_rect(ctm,
+                                         pdf_bound_page(doc->document, page)));
 
-	pix = fz_new_pixmap_with_rect(fz_device_rgb, bbox);
-	fz_clear_pixmap_with_color(pix, 0xff);
+  pix = fz_new_pixmap_with_bbox(doc->context, fz_device_rgb, bbox);
+  fz_clear_pixmap_with_value(doc->context, pix, 0xff);
 
-	dev = fz_new_draw_device(doc->glyphcache, pix);
-	fz_execute_display_list(list, dev, ctm, bbox);
-	fz_free_device(dev);
+  dev = fz_new_draw_device(doc->context, pix);
+  fz_run_display_list(list, dev, ctm, bbox, NULL);
+  fz_free_device(dev);
 
-        doc->rows = pix->h;
-        doc->cols = pix->w;
-        buffer_size = doc->rows * doc->cols * bpp;
-        if ((buffer = malloc(buffer_size)) == NULL) {
-          return NULL;
-        }
-        memset(buffer, 0, buffer_size);
-        for (y = 0; y < doc->rows; y++) {
-          for (x = 0; x < doc->cols; x++) {
-            unsigned char *s = pix->samples + ((y * doc->cols + x) << 2);
-            fbval_t *d = (fbval_t *)(buffer + (y * doc->cols + x) * bpp);
-            *d = FB_VAL(s[0], s[1], s[2]);
-          }
-        }
-	fz_drop_pixmap(pix);
-	fz_free_display_list(list);
-	pdf_free_page(page);
-	pdf_age_store(doc->xref->store, 3);
-	return buffer;
+  doc->rows = fz_pixmap_height(doc->context, pix);
+  doc->cols = fz_pixmap_width(doc->context, pix);
+  buffer_size = doc->rows * doc->cols * bpp;
+  if ((buffer = malloc(buffer_size)) == NULL) {
+    fz_free_context(doc->context);
+    return NULL;
+  }
+  memset(buffer, 0, buffer_size);
+  for (y = 0; y < doc->rows; y++) {
+    for (x = 0; x < doc->cols; x++) {
+      unsigned char *s = fz_pixmap_samples(doc->context, pix) +
+        ((y * doc->cols + x) << 2);
+      fbval_t *d = (fbval_t *)(buffer + (y * doc->cols + x) * bpp);
+      *d = FB_VAL(s[0], s[1], s[2]);
+    }
+  }
+  fz_drop_pixmap(doc->context, pix);
+  fz_free_display_list(doc->context, list);
+  pdf_free_page(doc->document, page);
+  return buffer;
 }
 
-int doc_pages(struct doc *doc)
-{
-	return pdf_count_pages(doc->xref);
+int doc_pages(struct doc *doc) {
+  return pdf_count_pages(doc->document);
 }
 
-struct doc *doc_open(char *path)
-{
-	struct doc *doc = malloc(sizeof(*doc));
-	fz_accelerate();
-	doc->glyphcache = fz_new_glyph_cache();
-	if (pdf_open_xref(&doc->xref, path, NULL)) {
-		free(doc);
-		return NULL;
-	}
-	if (pdf_load_page_tree(doc->xref)) {
-		free(doc);
-		return NULL;
-	}
-        doc->outline = convert_outline(pdf_load_outline(doc->xref));
-	return doc;
+struct doc *doc_open(char *path) {
+  struct doc *doc = malloc(sizeof(*doc));
+  doc->context = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+  if ((doc->document = pdf_open_document(doc->context, path)) != NULL) {
+    doc->outline = convert_outline(pdf_load_outline(doc->document));
+    return doc;
+  } else {
+    fz_free_context(doc->context);
+    free(doc);
+    return NULL;
+  }
 }
 
 void doc_close(struct doc *doc)
 {
-        free_outline(doc->outline);
-	pdf_free_xref(doc->xref);
-	fz_free_glyph_cache(doc->glyphcache);
-	free(doc);
+  free_outline(doc->outline);
+  pdf_close_document(doc->document);
+  free(doc);
+  fz_free_context(doc->context);
 }
 
 void doc_geometry(struct doc *doc, int *rows, int *cols) {
-        *rows = doc->rows;
-        *cols = doc->cols;
+  *rows = doc->rows;
+  *cols = doc->cols;
 }
 
 struct outline *doc_outline(struct doc *doc) {
@@ -142,7 +138,6 @@ struct outline *doc_outline(struct doc *doc) {
 }
 
 int doc_lookup_page(struct doc *doc, struct outline *it) {
-  pdf_link *link = ((pdf_outline *)(it->data))->link;
-  return pdf_find_page_number(doc->xref, fz_array_get(link->dest, 0)) + 1;
+  return ((fz_outline *)(it->data))->dest.ld.gotor.page + 1;
 }
 
