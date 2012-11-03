60d4735ad40a36d5323ceba4d2d8ce09672b156d
Chuan Ji <jichuan89@gmail.com>
2012-11-03 01:18:44 -0700

    Begin framebuffer abstraction.

---
+++ framebuffer.cpp
@@ -0,0 +1,146 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                           *
+ *  Copyright (C) 2012 Chuan Ji <jichuan89@gmail.com>                        *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *   http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// This file implements the framebuffer abstraction.
+
+#include "framebuffer.hpp"
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+const char * const Framebuffer::DEFAULT_FRAMEBUFFER_DEVICE = "/dev/fb0";
+
+Framebuffer *Framebuffer::Open(const std::string &device) {
+  Framebuffer *fb = new Framebuffer();
+  if ((fb->_fd = open(device.c_str(), O_RDWR)) != -1) {
+    if ((ioctl(fb->_fd, FBIOGET_VSCREENINFO, &(fb->_vinfo)) != -1) &&
+        (ioctl(fb->_fd, FBIOGET_FSCREENINFO, &(fb->_finfo)) != -1)) {
+      switch (fb->GetDepth()) {
+       case 1:
+        fb->_write_pixel_impl = &Framebuffer::WritePixel1;
+        break;
+       case 2:
+        fb->_write_pixel_impl = &Framebuffer::WritePixel2;
+        break;
+       case 3:
+        fb->_write_pixel_impl = &Framebuffer::WritePixel3;
+        break;
+       case 4:
+        fb->_write_pixel_impl = &Framebuffer::WritePixel4;
+        break;
+       default:
+        fprintf(stderr, "Unsupported color depth %d", fb->GetDepth());
+        abort();
+      }
+      if ((fb->_buffer = mmap(NULL, fb->GetBufferSize(), PROT_READ | PROT_WRITE,
+                              MAP_SHARED, fb->_fd, 0)) != MAP_FAILED) {
+        return fb;
+      }
+    }
+  }
+
+  delete fb;
+  perror("Error initializing framebuffer");
+  return NULL;
+}
+
+Framebuffer::~Framebuffer() {
+  if (_buffer != MAP_FAILED) {
+    munmap(_buffer, GetBufferSize());
+  }
+  if (_fd != -1) {
+    close(_fd);
+  }
+}
+
+Framebuffer::Framebuffer() {
+  // Detect endian-ness.
+  int x = 1;
+  _little_endian = (reinterpret_cast<unsigned char *>(&x))[0];
+}
+
+int Framebuffer::GetBufferSize() const {
+  return _finfo.smem_len;
+}
+
+int Framebuffer::GetDepth() const {
+  return (_vinfo.bits_per_pixel + 7) >> 3;
+}
+
+void Framebuffer::WritePixel(int x, int y, int r, int g, int b) {
+  assert(GetDepth() <= sizeof(unsigned int));
+  unsigned int v = ((r >> (8 - _vinfo.red.length)) << _vinfo.red.offset) |
+                   ((g >> (8 - _vinfo.green.length)) << _vinfo.green.offset) |
+                   ((b >> (8 - _vinfo.blue.length)) << _vinfo.blue.offset);
+  (this->*_write_pixel_impl)(v, GetPixelAddress(x, y));
+}
+
+void *Framebuffer::GetPixelAddress(int x, int y) const {
+  return reinterpret_cast<unsigned char *>(_buffer) +
+      (y + _vinfo.yoffset) * _finfo.line_length +
+      (x + _vinfo.xoffset) * GetDepth();
+}
+
+void Framebuffer::WritePixel1(unsigned int v, void *dest) {
+  *(reinterpret_cast<unsigned char *>(dest)) = static_cast<unsigned char>(v);
+}
+
+void Framebuffer::WritePixel2(unsigned int v, void *dest) {
+  assert(sizeof(unsigned short) == 2);
+  *(reinterpret_cast<unsigned short *>(dest)) = static_cast<unsigned short>(v);
+}
+
+void Framebuffer::WritePixel3(unsigned int v, void *dest) {
+  assert(sizeof(unsigned short) == 2);
+  if (_little_endian) {
+    *(reinterpret_cast<unsigned short *>(dest)) =
+        static_cast<unsigned short>(v);
+    *(reinterpret_cast<unsigned char *>(dest) + 2) =
+        static_cast<unsigned char>(v >> 16);
+  } else {
+    *(reinterpret_cast<unsigned short *>(dest)) =
+        static_cast<unsigned short>(v >> 8);
+    *(reinterpret_cast<unsigned char *>(dest) + 2) =
+        static_cast<unsigned char>(v);
+  }
+}
+
+void Framebuffer::WritePixel4(unsigned int v, void *dest) {
+  assert(sizeof(unsigned int) == 4);
+  *(reinterpret_cast<unsigned int *>(dest)) = static_cast<unsigned int>(v);
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+  Framebuffer *fb = Framebuffer::Open();
+  for (int i = 0; i < 1000; ++i) {
+    for (int y = 0; y < 600; ++y) {
+      for (int x = 0; x < 800; ++x) {
+        fb->WritePixel(x, y, i % 0x100, i % 0x100, i % 0x100);
+      }
+    }
+  }
+  delete fb;
+}
---
+++ framebuffer.hpp
@@ -0,0 +1,74 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                           *
+ *  Copyright (C) 2012 Chuan Ji <jichuan89@gmail.com>                        *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *   http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// This file declares the framebuffer abstraction.
+
+#ifndef FRAMEBUFFER_HPP
+#define FRAMEBUFFER_HPP
+
+#include <linux/fb.h>
+#include <string>
+
+// An abstraction for a framebuffer device.
+class Framebuffer {
+ public:
+  static const char * const DEFAULT_FRAMEBUFFER_DEVICE;
+  // Factory method to initialize a framebuffer device and returns an
+  // abstraction object. Returns NULL if the initialization failed. Caller owns
+  // returned object.
+  static Framebuffer *Open(
+      const std::string &device = DEFAULT_FRAMEBUFFER_DEVICE);
+  virtual ~Framebuffer();
+  // Returns the size of the mmap'd buffer.
+  int GetBufferSize() const;
+  // Returns the color depth, i.e., number of bytes per pixel.
+  int GetDepth() const;
+  // Set a pixel on the framebuffer.
+  void WritePixel(int x, int y, int r, int g, int b);
+ private:
+  // File descriptor of the opened framebuffer device.
+  int _fd;
+  // Framebuffer info structures.
+  fb_var_screeninfo _vinfo;
+  fb_fix_screeninfo _finfo;
+  // Whether we're on a little-endian system.
+  bool _little_endian;
+  // mmap'd buffer.
+  void *_buffer;
+  // Handle to WritePixel[1-4], depending on present bpp.
+  void (Framebuffer::*_write_pixel_impl)(unsigned int v, void *dest);
+  // Contructors are disallowed. Use factory method Open() instead.
+  Framebuffer();
+  // No copying is allowed.
+  Framebuffer(const Framebuffer &);
+  Framebuffer &operator = (const Framebuffer &);
+  // Returns the position within the buffer of the pixel at (x, y).
+  void *GetPixelAddress(int x, int y) const;
+  // Writes a pixel value to a location, assuming 8bpp.
+  void WritePixel1(unsigned int v, void *dest);
+  // Writes a pixel value to a location, assuming 16bpp.
+  void WritePixel2(unsigned int v, void *dest);
+  // Writes a pixel value to a location, assuming 24bpp.
+  void WritePixel3(unsigned int v, void *dest);
+  // Writes a pixel value to a location, assuming 32bpp.
+  void WritePixel4(unsigned int v, void *dest);
+  friend int main();
+};
+
+#endif
+
